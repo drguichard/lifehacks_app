6c5db0ee98f903d1eea3ef5ec24ffd7f71038f2b
TG <tguichard8@hotmail.com>
2019-12-04 16:29:31 +0100

    tip views index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'tips/index'
-  get 'tips/show'
+  root to: "tips#index"
   devise_for :users
+  resources :tips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
