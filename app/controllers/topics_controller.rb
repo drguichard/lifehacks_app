fcdcc33d64b9e96c75b1100128d41121429380c0
TG <tguichard8@hotmail.com>
2019-12-05 17:01:35 +0100

    created all topics controller methods

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,40 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic=.update(topic.params)
+    redirect_to topics_path
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_path
+  end
+
+  def create
+    @topic = Topic.create(topic_params)
+    redirect_to topic_path(@topic.id)
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  private
+
+  def topic_params 
+    params.require(:topic).permit(:name)
+
+end
