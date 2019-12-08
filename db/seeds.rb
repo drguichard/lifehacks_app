7b250b03030ca4104207fa0d0eab63df06b3591a
TG <tguichard8@hotmail.com>
2019-12-09 00:02:53 +0100

    various front improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,10 +64,10 @@ p "---------------- TIPS ----------------"
 tips = Tip.create!([
 	{user_id:"11", topic_id:"1", name:'Quand planter ?', content: "Au mois de mai à 20 cm de profondeur dans du terreau et avec une bonne exposition"},
 	{user_id:"11", topic_id:"2", name:'S\'hydrater', content:'le mal de tête est souvent causé par la déshydration'},
-	{user: User.all.sample, topic: Topic.all.sample, name:'Proteger les jeunes plants', content:'Mettre de l\'anti-limace ou entourer de cendre et renouveler après la pluie.'},
-	{user: User.all.sample, topic: Topic.all.sample, name:'Faire des listes', content:'lister ses choses à faire, les plus urgentes en premier et les moins urgentes à la fin; utiliser des applis telles que Trello'},
-	{user: User.all.sample, topic: Topic.all.sample, name:'Boire quali', content:'privilégier des alcools de qualités sans sulfites'},
-	{user: User.all.sample, topic: Topic.all.sample, name:'Eviter les mélanges', content:'ca c\'est bien connu hein, tu le sais mais tu le fais quand même'}
+	{user: User.all.sample, topic_id:"1", name:'Proteger les jeunes plants', content:'Mettre de l\'anti-limace ou entourer de cendre et renouveler après la pluie.'},
+	{user: User.all.sample, topic_id:"3", name:'Faire des listes', content:'lister ses choses à faire, les plus urgentes en premier et les moins urgentes à la fin; utiliser des applis telles que Trello'},
+	{user: User.all.sample, topic_id:"2", name:'Boire quali', content:'privilégier des alcools de qualités sans sulfites'},
+	{user: User.all.sample, topic_id:"2", name:'Eviter les mélanges', content:'ca c\'est bien connu hein, tu le sais mais tu le fais quand même'}
 ])
 tp Tip.all
 p "The database has now #{Tip.count} tips"
