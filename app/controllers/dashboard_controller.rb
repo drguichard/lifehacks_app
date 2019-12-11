66ac8c3435e2d89c5cb130006685915fd1b2a639
TG <tguichard8@hotmail.com>
2019-12-11 13:06:07 +0100

    all pages ok before heroku

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+  before_action :secret, only: [:edit, :update, :destroy]
 
   def edit
   end
@@ -8,15 +9,28 @@ class DashboardController < ApplicationController
 
   def show
   	  @user = current_user
+  	  @tips = @user.tips
   end
 
   def delete
   end
 
-      private
+   
 
-  #def user_params
-  #  params.require(:user).permit(:first_name, :last_name, :email)
-  #end
+
+ private
+
+ # def user_params
+ #   params.require(:user).permit(:user_id)
+ # end
+#
+  def secret
+    @tip = Tip.find(params[:id])
+    @admin = User.find(@tip.user_id)
+      unless @admin.id == current_user.id
+        flash[:success] = "Vous n'avez pas le droit d'éditer ou supprimer le sujet car vous n'êtes pas l'auteur !"
+        redirect_to tips_path
+      end
+  end
 
 end
